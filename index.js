2f5ddc0a97c53ce09e4e184e10ecdbb8cf915ac7
camillebqt <camille.bouquet@cpe.fr>
2019-12-06 16:49:26 -0600

    oops

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const q = require('daskeyboard-applet');
 const logger = q.logger; 
 
-// skyscanner api url
+class FlyMe extends q.DesktopApp {
 
+    constructor() {
+        super();
+        // every minutes
+        this.pollingInterval = 60*1000; // ms
+        logger.info("Get the cheapest flight price!");
+    }
+
+    async run() {
+	}
+}   
+
+
+// skyscanner api url
 const API_BASE_URL = `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices`;
 
 function postMethod(){
@@ -11,7 +24,7 @@ function postMethod(){
 		method: 'POST',
 		headers: {
 			'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-			'x-rapidapi-key': "4cc1c2c4bcmsh83c89946482a731p1f65fajsnea2f98850273",
+			'x-rapidapi-key': `${apiKey}`,
 			'content-type': "application/json",
 			'location': `http://partners.api.skyscanner.net/apiservices/pricing/uk2/v1.0/${SESSION_KEY}`
 		},
@@ -29,28 +42,34 @@ function postMethod(){
 
 function getMethod(){
 	const settings = {
-		url: 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/4f8b1b0b-799a-414f-bf3f-48802cf83ce3?pageIndex=0&pageSize=10',
 		url: `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/${SESSION_KEY}?pageIndex=0&pageSize=10`,
 		// SESSION_KEY est recue dans "location" dans le headers lors de la creation de la session // TODO: comment recuperer cette valeur??
 		method: 'GET',
 		headers: {
 			'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-			'x-rapidapi-key': "4cc1c2c4bcmsh83c89946482a731p1f65fajsnea2f98850273"
+			'x-rapidapi-key': `${apiKey}`
 		}
 	}
 }
 
+// Goal: collect the price
 function getPrice(){
 	const settings = {
 		url: `${API_BASE_URL}/apiservices/browsequotes/v1.0/US/USD/en-US/${this.config.ORIGIN_PLACE}-sky/${this.config.DESTINATION_PLACE}-sky/${DEPART_DATE}?inboundpartialdate=${RETURN_DATE}`,
 		method: 'GET',
 		headers: {
 			"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-			"x-rapidapi-key": "4cc1c2c4bcmsh83c89946482a731p1f65fajsnea2f98850273"
+			"x-rapidapi-key": `${apiKey}`
 		}
 	}
 }
 
+
+module.exports = {
+    BirthdayReminder: BirthdayReminder
+}
+
+const applet = new BirthdayReminder();
 
 
 
